2bc08d6238f8ae4a0e87d78fa4c9fa451fc592ff
Richard  A <Richard@techni-core.com>
2018-08-06 21:57:01 -0500

    First commit

---
+++ Compliance/Pages/CompTool/ToolStart.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Compliance.Pages.CompTool
+{
+    public class ToolStartModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
